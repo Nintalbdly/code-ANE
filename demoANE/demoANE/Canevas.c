15898b6320090982c82c060c5a9441d61418ecd5
AlainBruceDaly Nintunze <alanintunze@etmlnet.local>
2024-11-08 15:55:43 +0100

    creation depot code

---
+++ demoANE/demoANE/Canevas.c
@@ -0,0 +1,75 @@
+//-----------------------------------------------------------------------------------//
+// Nom du projet 		: DemoSLO01 ANE
+// Nom du fichier 		: Demo ANe
+// Date de création 	: 19.09.2015
+// Date de modification : xx.xx.20xx
+//
+// Auteur 				: Philou (Ph. Bovey)
+//
+// Version				: x.x
+//
+// Description          : 
+//
+//
+// Remarques :            lien pour la table ASCII :
+// 						  -> http://www.asciitable.com/
+// 						  lien pour la saisie de clavier avec getc & getchar pour ne pas
+//                        avoir des erreurs d'interprétation
+// 						  -> http://fr.openclassrooms.com/informatique/cours/utiliser-les-bonnes-fonctions-d-entree
+// 						  lien vers différents
+// 						  -> http://fr.wikipedia.org/wiki/String.h
+//----------------------------------------------------------------------------------//
+//--appel de librairies standard
+
+#math
+//--appel de libraire
+#include<math.h>//
+
+//--appel headerfile ->
+
+
+
+#ifdef USE
+
+#endif // 
+
+
+//--prototye
+
+
+
+//--variable globale-WARNING a evite ici
+
+
+//CONSTANTE-WARNING a evite ici
+
+
+//--déclaration de fonction 
+
+
+//--fonction pricipal-> main
+//type_de _sortie mon_fvt(param_entree1,param_entree2,....)
+void main()
+{
+    //instruction1
+
+    //instruction 2
+
+
+    //instuction  3
+
+
+
+
+
+}
+
+//--- librairie standart ---//
+//#include <stdio.h>	// pour usage printf et scanf_s
+//                    // Nouveauté du VC++ 2005, 2008, 2010 et 2015 : le scanf_s remplace scanf
+//
+//int slo(void)
+//{
+//
+//  return(0);
+//}
---
+++ demoANE/demoANE/Demo slo.c
@@ -0,0 +1,84 @@
+//-----------------------------------------------------------------------------------//
+// Nom du projet 		: DemoSLO01 ANE
+// Nom du fichier 		: Demo ANe
+// Date de création 	: 19.09.2015
+// Date de modification : xx.xx.20xx
+//
+// Auteur 				: Philou (Ph. Bovey)
+//
+// Version				: x.x
+//
+// Description          : 
+//
+//
+// Remarques :            lien pour la table ASCII :
+// 						  -> http://www.asciitable.com/
+// 						  lien pour la saisie de clavier avec getc & getchar pour ne pas
+//                        avoir des erreurs d'interprétation
+// 						  -> http://fr.openclassrooms.com/informatique/cours/utiliser-les-bonnes-fonctions-d-entree
+// 						  lien vers différents
+// 						  -> http://fr.wikipedia.org/wiki/String.h
+//----------------------------------------------------------------------------------//
+//--appel de librairies standard
+//--appel de libraire
+#include<math.h>//
+#include<corecrt_math_defines.h>
+
+//--appel de libraire
+
+//--prototype WARNING à eviter
+
+ 
+
+//--appel headerfile ->
+
+
+
+
+
+//--prototye
+
+
+
+//--variable globale-WARNING a evite ici
+
+
+//CONSTANTE-WARNING a evite ici
+
+
+//--déclaration de fonction 
+
+
+//--fonction pricipal-> main
+//type_de _sortie mon_fvt(param_entree1,param_entree2,....)
+void mai()
+{
+     //--déclaration de variable--//
+    //--type nom_variable
+
+    // type reel
+        int longeur_m, largeur_m;
+
+        // type reel
+        float surfaceCarre_m2;
+    
+    
+   //instruction1
+
+    //instruction 2
+
+
+    //instuction  3
+
+
+
+
+
+}
+
+//--- librairie standart ---//
+//#include <stdio.h>	// pour usage printf et scanf_s
+//                    // Nouveauté du VC++ 2005, 2008, 2010 et 2015 : le scanf_s remplace scanf
+//
+//int slo(void)
+//{
